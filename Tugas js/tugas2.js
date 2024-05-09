11ecb0deeccc3e0e1c6692c4c0e22ca888d4cdd0
Life-In-Rio <151702664+Life-In-Rio@users.noreply.github.com>
2024-05-09 11:52:19 +0700

    update tugas JS LAGI BROW

---
+++ Tugas js/tugas2.js
@@ -0,0 +1,78 @@
+//soal no 1
+let permen = 30;
+let permenandi = 30 - 10;
+console.log(permenandi);
+let permenbeli = 20 * 2;
+console.log(permenbeli);
+//sampe sini
+
+//soal no 2
+let namakucing = "ali" + "hasan";
+console.log(namakucing);
+//sampe sini
+
+//soal no 3
+let belalang = "12";
+let nyamuk = "11";
+let angsa = 10;
+console.log(+belalang + nyamuk);
+console.log(+angsa + +nyamuk);
+//sampai sini
+
+//sola no 4
+let bukufiksi = 5000;
+let jumlahbeli = 5;
+let belibuku1 = bukufiksi * jumlahbeli;
+console.log(belibuku1);
+
+let bukunonfiksi = 7000;
+let jumlahbeli2 = 3;
+let belibuku2 = bukunonfiksi * jumlahbeli2;
+console.log(belibuku2);
+
+let pena = 2000;
+let jumlahbeli3 = 2;
+let belipena = pena * jumlahbeli3;
+console.log(belipena);
+
+let totalsemuabarang = belipena + belibuku2 + belibuku1;
+console.log(totalsemuabarang);
+//sampai sini
+
+//soal no 5
+let A = 5 + 3 * 2;
+console.log(A);
+//sampe sini
+
+//soal no 6
+let kresek = 4;
+let kotak = 6;
+let ember = 5 + (kresek = kotak);
+console.log(ember);
+//sampe sini
+
+//soal no 7
+let z = 5 + 10 + 15;
+let x = z;
+let y = z;
+console.log(z);
+console.log(x);
+console.log(y);
+//sampe sini
+
+//soal no 8
+let produk = 4;
+let Produk = produk * 2;
+console.log(Produk);
+let PRoduk = Produk * 2;
+console.log(PRoduk);
+//sampe sini
+
+//soal no 9
+let X = 5;
+X++;
+console.log(X);
+let Y = 8;
+Y--;
+console.log(Y);
+//sampe sini
